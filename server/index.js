fc91230b75bc39535619f0036be3ab74ea509458
“JHuang19960705 <abc850705@gmail.com>
2024-01-17 22:37:51 +0800

    不要API

--- server/index.js
+++ server/index.js
@@ -28,9 +28,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api/user", authRoute);
+app.use("/user", authRoute);
 //要進去必須先登入
-app.use("/api/content",
+app.use("/content",
   passport.authenticate("jwt", { session: false }),
   contentRoute
 );
