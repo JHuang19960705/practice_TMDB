9cea837c40aa3e64bbf6a1b18949fb5fd51c15d1
“JHuang19960705 <abc850705@gmail.com>
2024-03-01 22:31:32 +0800

    try

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -33,18 +33,6 @@ router.get("/getUserById/:userId", async (req, res) => {
   }
 });
 
-// 透過Id拿到該會員recommend資料
-router.get("/getUserRecommendById/:userId", async (req, res) => {
-  let { userId } = req.params;
-  try{
-    let userFound = await User.findOne({ _id: userId }, { slide: 1, cast: 1, favoritePerson: 1, theme: 1, _id: 1, contentId: 1, theater: 1})
-      .exec();
-    return res.send(userFound);
-  } catch(e){
-    return res.status(500).send(e);
-  }
-});
-
 // 註冊
 router.post("/register", async (req, res) => {
   let { email, username, password, role } = req.body;
