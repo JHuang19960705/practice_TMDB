dbeccd93553c1c37e88920f19deb77d0f05c2f04
“JHuang19960705 <abc850705@gmail.com>
2024-04-05 02:00:49 +0800

    優化程式碼

--- client/src/pages/Back/Recommend/HandleSlide/CurrentSlide/CurrentSlide.js
+++ client/src/pages/Back/Recommend/HandleSlide/CurrentSlide/CurrentSlide.js
@@ -10,7 +10,7 @@ export default function CurrentSlide({ currentUser }) {
 
   const settings = {
     dots: false,
-    infinite: true,
+    infinite: currentUser.user.slide.tmdbImgBackdrop.length >= 2,
     speed: 2500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -37,32 +37,37 @@ export default function CurrentSlide({ currentUser }) {
     ]
   };
 
+  // 將圖片存在backdropElements中
+  let backdropElements = null;
+  if (currentUser && currentUser.user.slide.tmdbImgBackdrop) {
+    backdropElements = currentUser.user.slide.tmdbImgBackdrop.map((backdrop, index) => {
+      return (
+        <div key={index}>
+          <img src={tmdbBaseURL + backdrop} style={{ width: "100%", margin: "auto" }} />
+        </div>
+      );
+    });
+  };
+
   return (
     <div id="carousel-wrap">
       <Slider ref={slider} {...settings}>
-        {
-          // 渲染目前使用者的幻燈片 
-          currentUser && currentUser.user.slide.tmdbImgBackdrop &&
-          currentUser.user.slide.tmdbImgBackdrop.map((backdrop, index) => {
-            return (
-              <button key={backdrop} >
-                <img src={tmdbBaseURL + backdrop} alt={`Slide ${index + 1}`} style={{ width: "100%", margin: "auto" }} />
-              </button>)
-          })
-        }
+        {backdropElements}
       </Slider>
-      <div id="controls">
-        <span id="prev" onClick={() => slider?.current?.slickPrev()}>
-          <svg className="w-5 md:w-10 mx-1 text-blue-500 transform dark:text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-          </svg>
-        </span>
-        <span id="next" onClick={() => slider?.current?.slickNext()}>
-          <svg className="w-5 md:w-10 mx-1 text-blue-500 transform dark:text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-          </svg>
-        </span>
-      </div>
+      {currentUser && currentUser.user.slide.tmdbImgBackdrop && currentUser.user.slide.tmdbImgBackdrop.length >= 2 &&
+        <div id="controls">
+          <span id="prev" onClick={() => slider?.current?.slickPrev()}>
+            <svg className="w-5 md:w-10 mx-1 text-blue-500 transform dark:text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+            </svg>
+          </span>
+          <span id="next" onClick={() => slider?.current?.slickNext()}>
+            <svg className="w-5 md:w-10 mx-1 text-blue-500 transform dark:text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+            </svg>
+          </span>
+        </div>
+      }
     </div>
   )
 }
